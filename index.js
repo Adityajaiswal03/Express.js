dfc1278cc6973349ae3c411135e99674188e2786
Adityajaiswal03 <git@github.com:Adityajaiswal03/ProductService.git>
2024-07-10 15:42:52 +0530

    Server Established

--- index.js
+++
@@ -1,6 +0,0 @@
-const path=require('path');
-const p='/Users/adi/Documents/Study/Trimester 4/Development/Express/Course.txt'
-const dirName=path.dirname(p);
-const extention=path.extname(p);    
-console.log(dirName);
-console.log(extention);
